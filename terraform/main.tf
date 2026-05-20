5feb373fcdc613ef3d2be5fab069582b3aef7555
panky7 <panky7@users.noreply.github.com>
2026-05-20 23:43:42 +0530

    fix: use correct AllViewerExceptHostHeader Origin Request Policy ID in CloudFront

--- terraform/main.tf
+++ terraform/main.tf
@@ -267,18 +267,10 @@ resource "aws_cloudfront_distribution" "cdn" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "APIGateway-Backend"
 
-    forwarded_values {
-      query_string = true
-      headers      = ["*"]
-      cookies {
-        forward = "all"
-      }
-    }
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
+    origin_request_policy_id = "b689b0a8-53d0-40ab-baf2-68738e2966ac" # AllViewerExceptHostHeader
 
     viewer_protocol_policy = "https-only"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
   }
 
   restrictions {
