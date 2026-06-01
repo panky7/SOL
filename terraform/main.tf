c589eb404d745c5da96a82b9f0976b06b979df0a
panky7 <panky7@users.noreply.github.com>
2026-06-02 00:19:13 +0530

    fix: make reserved concurrency conditional on environment in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -198,7 +198,7 @@ resource "aws_lambda_function" "backend" {
   source_code_hash               = data.archive_file.dummy_lambda.output_base64sha256
   timeout                        = 30
   memory_size                    = 256
-  reserved_concurrent_executions = 5
+  reserved_concurrent_executions = local.env == "prod" ? 5 : null
 
   environment {
     variables = {
